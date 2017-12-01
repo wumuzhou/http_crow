c09f5fca3100bfbc69b576a23299a9d61c127fec
武目洲 <wumuzhou@jovision.com>
2017-12-01 14:40:02 +0800

    add some sql defines

--- HTTP_CROW_S/sql_work.hpp
+++ HTTP_CROW_S/sql_work.hpp
@@ -16,6 +16,24 @@
 #include<zdb/URL.h>
 #include <iostream>
 
+#define SQL_GET_STRING(result, parm1, field) \
+if(ResultSet_getStringByName(result, field)) \
+{ \
+	parm1 = ResultSet_getStringByName(result, field); \
+} \
+else{ \
+	parm1 = ""; \
+} \
+
+
+#define SQL_GET_INT(result, parm1, field) \
+if(ResultSet_getIntByName(result, field)) \
+{ \
+	parm1 = ResultSet_getIntByName(result, field); \
+} \
+else{ \
+	parm1 = 0; \
+} \
 
 
 class CSqlOpt
